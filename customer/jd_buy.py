7a0c7d59c2b2def18571326017c1b979e831f151
raaadford <993969300@qq.com>
2021-09-20 10:45:15 +0800

    [LDF] Create: file ip13_buy.py for lza T0.0.1 without test or debug. 2021/09/20

--- customer/jd_buy.py
+++ customer/jd_buy.py
@@ -26,15 +26,16 @@ def login(username, password):
 def buy_on_time(buytime):
     while True:
         now = datetime.datetime.now()
+
+        if driver.find_element_by_link_text("加入购物车").click() == 1:
+        # if driver.find_element_by_xpath("//*/ul[@class='nav_item']/li[text()='加入购物车']").click() == 1:  #this is another way.
+               print('There is ur Xbox Series S coming!')
+
         # if now.strftime('%Y-%m-%d %H:%M:%S') == buytime:
         #     driver.find_element_by_id('order-submit').click()
         #     time.sleep(3)
         #     print(now.strftime('%Y-%m-%d %H:%M:%S'))
         #     print('purchase success')
         # time.sleep(0.5)
-        if driver.find_element_by_link_text("加入购物车").click() == 1:
-        # if driver.find_element_by_xpath("//*/ul[@class='nav_item']/li[text()='加入购物车']").click() == 1:
-        print('There is ur Xbox Series S coming!')
-
 login('your_jd_username', 'your_jd_password')
 buy_on_time('2017-11-28 10:00:00')
